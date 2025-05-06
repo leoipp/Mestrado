978e9fa11f38c05a9516d056969e1e2ab3c03d89
Leonardo <leoippef@gmail.com>
2025-05-06 20:06:27 -0300

    up

--- Forecast/Predictive/DataConsistency.py
+++ Forecast/Predictive/DataConsistency.py
@@ -6,16 +6,26 @@ import matplotlib.pyplot as plt
 #%% Dataframe reader
 df = pd.ExcelFile(r"G:\PycharmProjects\Mestrado\Data\IFC_LiDAR_Plots_RTK.xlsx").parse("IFC - Plots Consistido")
 print(df.shape)
+
 #%% Scatter plot of VTCC vs Age (months)
-plt.figure(figsize=(10, 6))
-plt.scatter(df['Idade (meses)'], df['VTCC(m³/ha)'], alpha=0.7)
-plt.grid(linestyle='--', color='gray', alpha=0.5)
-plt.title('VTCC vs Age (months)')
-plt.xlabel('Age (months)')
-plt.ylabel('VTCC (m³/ha)')
-plt.show()
-#%% Histogram of VTCC and Age
 fig, axis = plt.subplots(1, 2, figsize=(10, 6))
+axis[0].scatter(df['Idade (meses)'], df['VTCC(m³/ha)'], alpha=0.7)
+axis[0].grid(linestyle='--', color='gray', alpha=0.5)
+axis[0].set_title('Age (months) vs VTCC')
+axis[0].set_xlabel('Age (months)')
+axis[0].set_ylabel('VTCC (m³/ha)')
+
+axis[1].scatter(df['Fustes (n)'], df['VTCC(m³/ha)'], alpha=0.7)
+axis[1].grid(linestyle='--', color='gray', alpha=0.5)
+axis[1].set_title('VTCC vs Stem')
+axis[1].set_xlabel('Stem (n)')
+axis[1].set_ylabel('VTCC (m³/ha)')
+
+plt.tight_layout()
+plt.show()
+
+#%% Histogram of VTCC and Age
+fig, axis = plt.subplots(1, 4, figsize=(14, 6))
 
 axis[0].hist(df['VTCC(m³/ha)'], bins=30, alpha=0.7, color='blue', zorder=2)
 axis[0].grid(linestyle='--', color='gray', alpha=0.5, zorder=1)
@@ -25,12 +35,27 @@ axis[0].set_ylabel('Frequency')
 
 axis[1].hist(df['Idade (meses)'], bins=30, alpha=0.7, color='blue', zorder=2)
 axis[1].grid(linestyle='--', color='gray', alpha=0.5, zorder=1)
-axis[1].set_title('Histogram of VTCC')
+axis[1].set_title('Histogram of Age')
 axis[1].set_xlabel('Age (months)')
 axis[1].set_ylabel('Frequency')
+
+axis[2].hist(df['Fustes (n)'], bins=30, alpha=0.7, color='blue', zorder=2)
+axis[2].grid(linestyle='--', color='gray', alpha=0.5, zorder=1)
+axis[2].set_title('Histogram of Stem')
+axis[2].set_xlabel('Stem (n)')
+axis[2].set_ylabel('Frequency')
+
+axis[3].hist(df['VTCC(m³/Fuste)'], bins=30, alpha=0.7, color='blue', zorder=2)
+axis[3].grid(linestyle='--', color='gray', alpha=0.5, zorder=1)
+axis[3].set_title('Histogram of ATV')
+axis[3].set_xlabel('ATV (m³/Stem)')
+axis[3].set_ylabel('Frequency')
+
+plt.tight_layout()
 plt.show()
+
 #%% Boxplot of VTCC by Age
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(14, 6))
 sns.boxplot(x='Idade (meses)', y='VTCC(m³/ha)', data=df, palette="Set2")
 plt.title('Boxplot of VTCC by Age (months)')
 plt.xlabel('Age (months)')
@@ -38,10 +63,12 @@ plt.ylabel('VTCC (m³/ha)')
 plt.xticks(rotation=45)
 plt.grid(linestyle='--', color='gray', alpha=0.5)
 plt.show()
+
 #%% Check for missing values
 missing_values = df.isnull().sum()
 print("Missing values in each column:")
 print(missing_values[missing_values > 0])
+
 #%% Check for duplicates
 duplicates = df.duplicated().sum()
 print(f"Number of duplicate rows: {duplicates}")
@@ -62,12 +89,42 @@ if not negative_age.empty:
     print(f"Negative Age values found in {len(negative_age)} rows.")
     df = df[df['Idade (meses)'] >= 0]
     print("Negative Age values removed.")
-#%% Check for age values greater than 200 months
-age_greater_200 = df[df['Idade (meses)'] > 200]
-if not age_greater_200.empty:
-    print(f"Age values greater than 200 months found in {len(age_greater_200)} rows.")
-    df = df[df['Idade (meses)'] <= 200]
-    print("Rows with Age greater than 200 months removed.")
+
+#%% Check for age values lower than 200 months and greater than 36 months
+age_outliers = df[(df['Idade (meses)'] < 36) | (df['Idade (meses)'] > 200)]
+# Check and remove if any outliers exist
+if not age_outliers.empty:
+    print(f"Found {len(age_outliers)} rows with age < 36 or > 200 months.")
+    df = df[(df['Idade (meses)'] >= 36) & (df['Idade (meses)'] <= 200)]
+    print("These rows have been removed from the DataFrame.")
+plt.figure(figsize=(10, 6))
+plt.scatter(df['Idade (meses)'], df['VTCC(m³/ha)'], alpha=0.7)
+plt.scatter(age_outliers['Idade (meses)'], age_outliers['VTCC(m³/ha)'], color='red', label='Extreme Lower Values', alpha=0.7)
+plt.grid(linestyle='--', color='gray', alpha=0.5)
+plt.title('VTCC vs Age (months) with Stem < 0.5')
+plt.xlabel('Age (months)')
+plt.ylabel('VTCC (m³/ha)')
+plt.legend()
+plt.show()
+#%% Check for stem values lower than 50%
+dim_split = df['ESPACAMENTO'].str.replace(',', '.').str.split('x', expand=True)
+df['ESP_AB'] = dim_split[0].astype(float) * dim_split[1].astype(float)
+df['Fustes (%)'] = df['Fustes (n)']/(df['Área.corrigida (m²)']/df['ESP_AB'])
+stem_outliers = df[(df['Fustes (%)'] < 0.5)]
+# Check and remove if any outliers exist
+if not stem_outliers.empty:
+    print(f"Found {len(stem_outliers)} rows with stem < 0.5%")
+    df = df[df['Fustes (%)'] >= 0.5]
+    print("These rows have been removed from the DataFrame.")
+plt.figure(figsize=(10, 6))
+plt.scatter(df['Idade (meses)'], df['VTCC(m³/ha)'], alpha=0.7)
+plt.scatter(stem_outliers['Idade (meses)'], stem_outliers['VTCC(m³/ha)'], color='red', label='Extreme Lower Values', alpha=0.7)
+plt.grid(linestyle='--', color='gray', alpha=0.5)
+plt.title('VTCC vs Age (months) with Stem < 0.5')
+plt.xlabel('Age (months)')
+plt.ylabel('VTCC (m³/ha)')
+plt.legend()
+plt.show()
 #%% Check for outliers using IQR (NOT RECOMMENDED - CHOOSE ZSCORE)
 Q1 = df['VTCC(m³/ha)'].quantile(0.25)
 Q3 = df['VTCC(m³/ha)'].quantile(0.75)
@@ -112,7 +169,7 @@ plt.ylabel('VTCC (m³/ha)')
 plt.legend()
 plt.show()
 #%% Check for extreme lower values - not removed with Z-score
-extreme_lower = df[(df["Idade (meses)"] >= 50) & (df['VTCC(m³/ha)'] < 100)]
+extreme_lower = df[df['VTCC(m³/ha)'] < 100]
 print(f"Extreme lower values found in {len(extreme_lower)} rows.")
 if not extreme_lower.empty:
     print("Extreme lower values found:")
@@ -128,7 +185,32 @@ plt.xlabel('Age (months)')
 plt.ylabel('VTCC (m³/ha)')
 plt.legend()
 plt.show()
+#%% Check for LiDAR values inconsistencies
+lidar_inconsistencies = df[(df['Elev P90'] <= 0) | (df['Elev variance'] < 0) | (df['Elev CURT mean CUBE'] < 0) | (df[
+    'Elev maximum'] > 50)]
+print(f"LiDAR inconsistencies found in {len(lidar_inconsistencies)} rows.")
+if not lidar_inconsistencies.empty:
+    print("LiDAR inconsistencies found:")
+    print(lidar_inconsistencies)
+    df = df[~df.index.isin(lidar_inconsistencies.index)]
+    print("LiDAR inconsistencies removed.")
+fig, axis = plt.subplots(1, 2, figsize=(10, 6))
+axis[0].scatter(df['Elev maximum'], df['Elev P99'], alpha=0.7)
+axis[0].scatter(lidar_inconsistencies['Elev maximum'], lidar_inconsistencies['Elev P95'], color='red', label='LiDAR Inconsistencies', alpha=0.7)
+axis[0].grid(linestyle='--', color='gray', alpha=0.5)
+axis[0].set_title('Elev maximum vs Elev P99')
+axis[0].set_xlabel('Elev maximum')
+axis[0].set_ylabel('Elev P99')
+axis[1].scatter(df['Elev maximum'], df['Elev P50'], alpha=0.7)
+axis[1].scatter(lidar_inconsistencies['Elev maximum'], lidar_inconsistencies['Elev P50'], color='red', label='LiDAR Inconsistencies', alpha=0.7)
+axis[1].grid(linestyle='--', color='gray', alpha=0.5)
+axis[1].set_title('Elev maximum vs Elev P50')
+axis[1].set_xlabel('Elev maximum')
+axis[1].set_ylabel('Elev P50')
+plt.show()
+
 #%% Export cleaned DataFrame to Excel
 output_file = r"G:\PycharmProjects\Mestrado\Data\IFC_LiDAR_Plots_RTK_Cleaned.xlsx"
 df.to_excel(output_file, index=False)
 print(f"Cleaned DataFrame exported to {output_file}")
+
--- Forecast/Predictive/VariablesCorrelation.py
+++ Forecast/Predictive/VariablesCorrelation.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import KFold, GridSearchCV
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 #%% Read the data
 df = pd.read_excel(r"G:\PycharmProjects\Mestrado\Data\IFC_LiDAR_Plots_RTK_Cleaned.xlsx")
@@ -23,9 +27,72 @@ correlations = correlations[abs(correlations) > 0.6]
 # Remove inventory variables
 correlations = correlations[correlations.index.str.contains('Elev')]
 print(correlations)
+#%% Recursive feature elimination - RFE
+feature_cols = df.columns[df.columns.str.contains('Elev')]
+X = df[feature_cols].values
+Y = df[['VTCC(m³/ha)']].values.ravel()
+# Cross-validation
+folds = KFold(n_splits=5, shuffle=True, random_state=100)
+hyper_params = [{'n_features_to_select': list(range(1, X.shape[1] + 1))}]
+# Base model
+model = RandomForestRegressor()
+# RFE and GridSearchCV
+rfe = RFE(estimator=model)
+model_cv = GridSearchCV(estimator=rfe, param_grid=hyper_params,
+                        scoring='neg_mean_squared_error',
+                        cv=folds, verbose=1, return_train_score=True)
+model_cv.fit(X, Y)
+# Features ranking
+print("\nRanking das features (ordenado):")
+ranking_sorted = sorted(zip(feature_cols,
+                            model_cv.best_estimator_.ranking_,
+                            model_cv.best_estimator_.support_),
+                        key=lambda x: x[1])
+for i, (name, rank, selected) in enumerate(ranking_sorted):
+    print(f"{i+1:02d}. {name:20s} | Rank: {rank:2d} | Selected: {selected}")
 #%% Plot the correlations with a heatmap
-subset = df[correlations.index.tolist() + ['VTCC(m³/ha)', 'Idade (meses)']]
-plt.figure(figsize=(10, 8))
-sns.heatmap(subset.corr(), cmap="YlGnBu", annot=True, mask=np.triu(np.ones_like(subset.corr())), square=True, cbar_kws={"shrink": .8})
+selected_cols = feature_cols[model_cv.best_estimator_.support_]
+corr_matrix = df[selected_cols].corr()
+plt.figure(figsize=(14, 12))
+sns.heatmap(
+    corr_matrix,
+    cmap="YlGnBu",
+    annot=True,
+    fmt=".2f",
+    annot_kws={"size": 5},  # reduz o tamanho da fonte
+    mask=np.triu(np.ones_like(corr_matrix, dtype=bool)),
+    square=True,
+    cbar_kws={"shrink": .8}
+)
+plt.xticks(rotation=45, ha='right')
+plt.title('Correlation Heatmap')
+plt.show()
+#%% Variance Inflation Factor (VIF)
+X_sel = df[selected_cols]
+vif_data = pd.DataFrame()
+vif_data['Feature'] = X_sel.columns
+vif_data['VIF'] = [variance_inflation_factor(X_sel.values, i) for i in range(X_sel.shape[1])]
+vif_data.to_excel(r"G:\PycharmProjects\Mestrado\Data\VIF.xlsx", index=False)
+print(vif_data.sort_values(by='VIF', ascending=False))
+#%% Assuming variables of chosen (Elev P90, Elev variance, Elev CURT mean CUBE, Idade (meses))
+X_sel = df[['Elev P90', 'Elev variance', 'Elev CURT mean CUBE', 'Idade (meses)']]
+vif_data = pd.DataFrame()
+vif_data['Feature'] = X_sel.columns
+vif_data['VIF'] = [variance_inflation_factor(X_sel.values, i) for i in range(X_sel.shape[1])]
+#vif_data.to_excel(r"G:\PycharmProjects\Mestrado\Data\VIF.xlsx", index=False)
+print(vif_data.sort_values(by='VIF', ascending=False))
+#%% Plot the correlations with a heatmap of chosen variables (Elev P90, Elev variance, Elev CURT mean CUBE, Idade (meses))
+corr_matrix = df[['Elev P90', 'Elev variance', 'Elev CURT mean CUBE', 'Idade (meses)']].corr()
+plt.figure(figsize=(14, 12))
+sns.heatmap(
+    corr_matrix,
+    cmap="YlGnBu",
+    annot=True,
+    fmt=".2f",
+    mask=np.triu(np.ones_like(corr_matrix, dtype=bool)),
+    square=True,
+    cbar_kws={"shrink": .8}
+)
+plt.xticks(rotation=45, ha='right')
 plt.title('Correlation Heatmap')
 plt.show()
